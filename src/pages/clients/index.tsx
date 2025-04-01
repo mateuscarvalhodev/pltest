b489942b973c46735710af44445945fcb5b05ceb
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2025-04-01 19:57:58 -0300

    chore(deps): update and unify lockfile entries for clsx, debug and add vite-tsconfig-paths

--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -1,7 +1,25 @@
-export default function Clients() {
+import { Payment, columns } from './columns'
+import { DataTable } from './data-table'
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function DemoPage() {
+  const data = await getData()
+
   return (
-    <>
-      oi
-    </>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
   )
 }
